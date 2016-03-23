a52abc75462a96117e6d97ebc7297b4571e8735c
Jessica Ayunani <j.ayunani@gmail.com>
2016-03-22 19:04:02 -0700

    Create everythingBeTrue.js

---
+++ everythingBeTrue.js
@@ -0,0 +1,13 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection 
+// (first argument).
+
+// Remember, you can access object properties through either dot notation or [] notation.
+
+function every(collection, pre) {
+  for(var i = 0; i < collection.length; i++) {
+  	if(!Boolean(collection[i][pre])) {
+  		return false;
+  	}
+  }
+  return true;
+}
